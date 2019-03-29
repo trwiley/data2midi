f4fc3e96b1fa17194436484de8decb55e8d0a8ef
Taylor Wiley <taylor.r.wiley@gmail.com>
2019-03-29 07:28:05 -0400

    Changed the version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='taylor.r.wiley@gmail.com',
     packages=['data2midi'],
     install_requires=['numpy', 'pandas', 'sklearn', 'MIDIUtil'],
-    version='0.1',
+    version='0.1.2',
     license='GNU GPL',
     description='simple library for sonifying tabular numeric data by converting it into a MIDI file.'
 )
